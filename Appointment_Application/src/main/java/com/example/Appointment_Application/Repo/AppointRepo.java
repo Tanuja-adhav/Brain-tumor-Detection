1100e494a40d5ff4c98f115d588d3cdbc402e72e
Aher Monika Santosh <157315937+ahermonika169@users.noreply.github.com>
2025-04-25 23:16:57 +0530

    Delete Appointment_Application directory

--- Appointment_Application/src/main/java/com/example/Appointment_Application/Repo/AppointRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.Appointment_Application.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Appointment_Application.entity.Appointment;
-
-public interface AppointRepo extends JpaRepository<Appointment, Integer> {
-
-}
